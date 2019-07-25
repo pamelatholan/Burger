7ecc892d97bd4bf3d8b40f1ea4bd47712ce4af38
Pam Tholan <pamelatholan@yahoo.com>
2019-07-25 12:04:17 -0400

    added gitignoe, connection and orm

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+
+//methods needed to retrieve and store data in database.
+
+selectAll()
+insertOne()
+updateOne()
+
+
+
+
+
+
+
+module.exports = orm;
