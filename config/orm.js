6f45ca10d49bb121be8535dc0cf349169fa66185
Pam Tholan <pamelatholan@yahoo.com>
2019-07-25 12:17:49 -0400

    added env, connection.js and orm.js

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+//Methods to use to retrieve and store data for DB
+selectAll()
+insertOne()
+updateOne()
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
