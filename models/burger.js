80948b31820db5185e0a4e22abb6d5bf9996c57e
Pam Tholan <pamelatholan@yahoo.com>
2019-07-29 11:45:23 -0400

    added and adjusted previous coding

--- models/burger.js
+++ models/burger.js
@@ -1,25 +1,30 @@
-// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
-
+// The code that will call the ORM functions using burger specific input for the ORM.
 var burger = {
-    all: function(cb) {
-      orm.all("burgers", function(res) {
-        cb(res);
-      });
+    // Display all burgers in the db.
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
     },
-    // The variables cols and vals are arrays.
-    create: function(cols, vals, cb) {
-      orm.create("burgers", cols, vals, function(res) {
-        cb(res);
-      });
+    // Add a new burger to the db.
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
     },
-    update: function(objColVals, condition, cb) {
-      orm.update("burgers", objColVals, condition, function(res) {
-        cb(res);
-      });
+    // Change the devoured status to true.
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    // Delete a burger from the db.
+    deleteOne: function(condition, cb) {
+        orm.deleteOne("burgers", condition, function(res) {
+            cb(res);
+        });
     }
-  };
-  
-
+};
 // Export the database functions for the controller (catsController.js).
 module.exports = burger;
