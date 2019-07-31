b7eb4454a82321c4915b70a35e9af3ef3e14b26c
Pam Tholan <pamelatholan@yahoo.com>
2019-07-30 21:35:15 -0400

    updated pkg.json

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require('path');
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
