2ef6f93733051676382b3154ebd0a8db0bf1db43
Pam Tholan <pamelatholan@yahoo.com>
2019-07-30 21:42:32 -0400

    updated server.js listener

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+    console.log("Server listening on: " + PORT);
 });
   
